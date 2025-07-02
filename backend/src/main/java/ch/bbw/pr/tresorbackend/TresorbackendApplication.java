71d6d1ae153c54196c50dbd69be88ff331a8da46
Jannis Milz <67200746+jannismilz@users.noreply.github.com>
2025-07-02 15:57:49 +0200

    Organize imports

--- backend/src/main/java/ch/bbw/pr/tresorbackend/TresorbackendApplication.java
+++ backend/src/main/java/ch/bbw/pr/tresorbackend/TresorbackendApplication.java
@@ -1,9 +1,7 @@
 package ch.bbw.pr.tresorbackend;
 
-import ch.bbw.pr.tresorbackend.model.ConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class TresorbackendApplication {
